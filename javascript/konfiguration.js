02d35bab1e8e2fbebad6d8c420673619b9433805
Oeberg <60393289+Oeberg@users.noreply.github.com>
2020-05-14 10:01:54 +0200

    konfiguration

--- javascript/konfiguration.js
+++ javascript/konfiguration.js
@@ -4,22 +4,22 @@ var modal = document.getElementById("Konfiguration1");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let closeModalClasses = document.getElementsByClassName("close-modal");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+for (let i = 0; i < closeModalClasses.length; i++) {
+  closeModalClasses[i].onclick = function() {
     modal.style.display = "none";
   }
 }
