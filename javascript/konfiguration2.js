664c765d5674a2bc34ed4f27b4c59358a08e20c4
Oeberg <60393289+Oeberg@users.noreply.github.com>
2020-05-14 22:26:30 +0200

    konfiguration del 2

---
+++ javascript/konfiguration2.js
@@ -0,0 +1,44 @@
+// Get the modal
+var modal = document.getElementById("Konfiguration-s-2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn-s-2");
+
+let closeModalClasses = document.getElementsByClassName("close-modal-s-2");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+for (let i = 0; i < closeModalClasses.length; i++) {
+  closeModalClasses[i].onclick = function() {
+    modal.style.display = "none";
+  }
+}
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
