0238ec2432c0e347bd66f5578bd07a362a5670a9
Lucas <60382112+Lukoly@users.noreply.github.com>
2020-05-18 16:00:20 +0200

    logo, status bar

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -17,3 +17,17 @@ window.onclick = function(event) {
     }
   }
 }
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
