e1183474202962f69d64ae7b175cba6cfb6aeaf9
Oeberg <60393289+Oeberg@users.noreply.github.com>
2020-05-28 19:08:59 +0200

    Update konfiguration.js

--- javascript/konfiguration.js
+++ javascript/konfiguration.js
@@ -1,27 +1,27 @@
 // Get the modal. Variablen har et unikt navn:modal
 var modal = document.getElementById("Konfiguration1");
 
-// Get the button that opens the modal
+// Get the button that opens the modal. # er kun med en queryselector
 var tandhjul = document.querySelector("#tandhjul")
 
 
-// Ændrer værdien til Lukas' knap navn (Class name)
+// en slags reference til html værdien
 let closeModalClasses = document.getElementsByClassName("close-modal");
-// Den tager kun det første element som defineres i det i parantese
+// beholdere til værdierne
 let modalS1 = document.querySelector(".modal-s-1");
 let modalS2 = document.querySelector(".modal-s-2");
 
 let manuelKonfiguration = document.querySelector("#manuel");
 
 
-// When the user clicks on the button on first modal vindue, open the modal2
+// When the user clicks on the button on first modal vindue, open the modal2. eventet specificerer noget der skal ske
 tandhjul.onclick = function(event) {
   modal.style.display = "block";
   modalS1.style.display = "block";
   modalS2.style.display = "none";
 }
 
-// fjernes denne, forsvinder tandhjuls functionen
+// 
 manuelKonfiguration.onclick = function() {
 modalS1.style.display = "none";
 modalS2.style.display = "block";
@@ -42,8 +42,8 @@ window.onclick = function(event) {
 
 
 
-// For loop der itererer igennem alle classer med close-modal classen
-// når der klikkes på et af de elementer lukkes modal'en
+// For loop, der itererer igennem alle classer med close-modal classen. et control statement.
+// når der klikkes på et af de elementer lukkes modal'en. length betyder kun til antallet af classes i denne kontekst.
 for (let i = 0; i < closeModalClasses.length; i++) {
   // let i = iterator variablen, som holder styr på hvor langt man er i ens iteration
   closeModalClasses[i].onclick = function() {
